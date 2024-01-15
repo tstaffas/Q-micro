ed441f548b80652dfeab73072a0661f28d1c6d3b
Julia <118765749+deblargue@users.noreply.github.com>
2024-01-15 16:30:07 +0100

    Add files via upload

---
+++ swabian/Analysis/newnewnew/Swabian_Microscope_analysis_0.1.0.py
@@ -0,0 +1,90 @@
+#------IMPORTS-----
+import Swabian_Microscope_library as Q
+
+# ------------ PARAMETERS AND CONSTANTS --------------
+eta_recipe = 'Swabian_multiframe_recipe_bidirectional_segments_0.0.4.eta'        # 'microscope_bidirectional_segments_0.0.3.eta'
+#eta_recipe = 'Swabian_multiframe_recipe_bidirectional_segments_0.0.6.eta'        # 'microscope_bidirectional_segments_0.0.3.eta'
+
+# Parameters to locate timeres files:
+folder = "/"             # Note: this is used to find the timeres file --> WRITE in your own data folder location
+clue = "nr_6"                    # Note: this is used to help find the correct timeres file when only given frequency (ex: 'higher_power', 'digit_8', '13h44m23s')
+#       ^ex. for data in "230828": {"digit_6"}
+
+timetag_file = 'nr_6_dup_marker_sineFreq(1)_numFrames(2)_sineAmp(0.3)_stepAmp(0.3)_stepDim(100)_date(231103)_time(14h48m42s).timeres'               # Note: let this be None if you want to use "clue" and "folder" to automatically find your file based on frequency
+#timetag_file = 'Data/231103/nr_6_dup_marker_sineFreq(1)_numFrames(2)_sineAmp(0.3)_stepAmp(0.3)_stepDim(100)_date(231103)_time(14h48m42s).timeres'
+#timetag_file = 'Data/231103/nr_6_dup_marker_sineFreq(10)_numFrames(2)_sineAmp(0.3)_stepAmp(0.3)_stepDim(100)_date(231103)_time(14h52m37s).timeres'
+#timetag_file = 'Data/231103/nr_6_dup_marker_sineFreq(10)_numFrames(10)_sineAmp(0.3)_stepAmp(0.3)_stepDim(100)_date(231103)_time(15h02m31s).timeres'
+#timetag_file = 'Data/231103/nr_6_dup_marker_sineFreq(50)_numFrames(25)_sineAmp(0.3)_stepAmp(0.3)_stepDim(100)_date(231103)_time(15h09m13s).timeres'
+#timetag_file = 'Data/231103/nr_6_dup_marker_sineFreq(50)_numFrames(50)_sineAmp(0.3)_stepAmp(0.3)_stepDim(100)_date(231103)_time(15h06m05s).timeres'
+
+# Scan parameters:
+nr_frames = 2        # OBS: SET VALUE TO USE DATAFILE
+freq = 1            # OBS: SET VALUE TO USE DATAFILE
+ampX = 0.3                      # --> step values between -0.3 and 0.3
+ampY = 0.3                      # --> sine values between -0.3 and 0.3
+dimX = 100                      # how many (stepwise) steps we take in scan
+bins = 20000                    # how many bins/containers we get back for one period   #20000 is good --> 10k per row
+ch_sel = "h2"
+
+# Playback Frame Rates for GIFs:
+# (time for one frame) = (number of steps)*(period) = (dimX)/(frequency)
+scan_fps = freq / dimX              # frame rate = (1/(time for one frame)) = (freq/dimX)
+gif_rates = [1, 5, scan_fps]      # playback frame rates for each gif we want to create
+gif_notes = ["", "", "(live)"]      # notes for gif we want (for each playback frame rate)
+
+# ------------ MISC. RELATIONSHIPS ------------
+dimY = int(round(dimX*(ampY/ampX)))     # How many pixels we want to use TODO: Get new data (where ampX != ampY) and test this relationship!
+freq_ps = freq * 1e-12                  # frequency scaled to unit picoseconds (ps)
+period_ps = 1/freq_ps                   # period in unit picoseconds (ps)
+binsize = int(round(period_ps/bins))    # how much time (in ps) each histogram bin is integrated over (=width of bins). Note that the current recipe returns "bins" values per period.
+
+print(f"bins = {bins},  binsize = {binsize}")  # *{10e-12} picoseconds")
+print(f"single frame scan time: {dimX / freq} sec,  scan frame rate: {scan_fps} fps")
+
+# NOTE: Below is a dictionary with all the parameters defined above. This way we can sent a dict with full access instead of individual arguments
+const = {
+    "eta_recipe"       : eta_recipe,
+    "timetag_file"     : timetag_file,
+    "clue"             : clue,
+    "folder"           : folder,
+    "nr_frames"        : nr_frames,
+    "freq"             : freq,
+    "ampX"             : ampX,
+    "ampY"             : ampY,
+    "dimX"             : dimX,
+    "bins"             : bins,
+    "ch_sel"           : ch_sel,
+    "dimY"             : dimY,
+    "freq_ps"          : freq_ps,
+    "period_ps"        : period_ps,
+    "binsize"          : binsize,
+    "scan_fps"         : scan_fps,
+    "gif_rates"        : gif_rates,
+    "gif_notes"        : gif_notes,
+}
+
+
+# --------- GET DATA AND HISTOGRAMS------------
+
+# quick version of "ad infinitum" code where we generate one image/frame at a time from ETA
+if __name__ == "__main__":
+
+    # --- GET TIMETAG FILE NAME, unless manually provided ---
+    if timetag_file is None:
+        timetag_file = Q.get_timres_name(folder, nr_frames, freq, clue=clue)
+        const["timetag_file"] = timetag_file
+    print(f"Using datafile: {timetag_file}\n")
+
+    # --- PROVIDE WHICH MAIN FOLDER WE SAVE ANY ANALYSIS TO (ex. images, raw data files, etc.), DEPENDING ON WHICH ETA FILE
+    st = timetag_file.find("date(")
+    fin = timetag_file.find(".timeres")
+    const["save_location"] = f"Analysis/({str(freq)}Hz)_{timetag_file[st:fin]}"   # This is the folder name for the folder where data, images, and anything else saved from analysis will be saved
+    #       FOR EXAMPLE: const["save_location"] = Analysis/(100Hz)_date(230717)_time(14h02m31s)
+
+    # --- EXTRACT AND ANALYZE DATA ---
+    #Q.eta_segmented_analysis_multiframe(const=const)   # note: all params we need are sent in with a dictionary. makes code cleaner
+
+    # testing prev version
+    Q.bap_eta_segmented_analysis_multiframe(const=const)   # note: all params we need are sent in with a dictionary. makes code cleaner
+
+
